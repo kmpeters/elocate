2cd4c3ac8a55dbd060b5d64f55712a036c8b1b6d
kpetersn <kpetersn@f095f811-4e88-6c84-d33e-e382bdb4531c>
2013-11-05 21:03:02 +0000

    Initial import of elocate.
    git-svn-id: https://subversion.xray.aps.anl.gov/kmp/elocate/trunk@503 f095f811-4e88-6c84-d33e-e382bdb4531c

---
+++ update.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#
+DB_DIR=/home/oxygen/KPETERSN/development/dservLocate/db
+#!DB_DIR=/net/s100dserv/xorApps/elocate/db
+#
+APSMASTER=/net/xmaster/export/APSmaster/gateway/epics
+APSSHARE=/net/s100dserv/APSshare/epics
+
+IGNORE_DIRS=`cat $DB_DIR/ignoreDirs | tr '\n' ' '`
+
+db_files=()
+
+### APSmaster
+if [ -e $APSMASTER ]
+then
+  echo "Updating database for APSmaster"
+  updatedb -l 0 -o $DB_DIR/APSmaster -n "$IGNORE_DIRS" -U $APSMASTER
+  db_files+=("$DB_DIR/APSmaster")
+fi
+
+### APSshare
+if [ -e $APSSHARE ]
+then
+  echo "Updating database for APSshare"
+  updatedb -l 0 -o $DB_DIR/APSshare -n "$IGNORE_DIRS" -U $APSSHARE
+  db_files+=("$DB_DIR/APSshare")
+fi
+
+### dservs
+for i in {1..35} 100
+do
+  DSV=s"$i"dserv
+  PTH=/net/"$DSV"/xorApps/epics
+  if [ -e $PTH ]
+  then
+    echo "Updating database for $DSV"
+    #!echo "updatedb -l 0 -o $DB_DIR/$DSV -n \"\$IGNORE_DIRS\" -U $PTH"
+    updatedb -l 0 -o $DB_DIR/$DSV -n "$IGNORE_DIRS" -U $PTH
+    db_files+=("$DB_DIR/$DSV")
+  fi 
+done
+
+### Make note of last update time
+date > $DB_DIR/lastUpdateTime
+### Save list of databases that were updated
+echo "${db_files[@]}" > $DB_DIR/lastUpdateList
